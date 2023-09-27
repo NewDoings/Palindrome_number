a35b9f91fe231d8cca02782c2b8836b2802433b4
NewDoings <87979577+NewDoings@users.noreply.github.com>
2023-09-27 12:00:15 +0300

    Add files via upload

---
+++ Palindrome_Number/Palindrome_Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalendrome(-1221));
+            Console.ReadKey();
+        }
+        public static bool IsPalendrome(int x)
+        {
+            bool palendrome = true;
+            //string array_x = x.ToString(); Можно создать одельную переменную для меньших датрат памяти, но будет выше рантайм
+            for (int i = 0; i < x.ToString().Length/2; i++)
+            {
+                if (x.ToString()[i] != x.ToString()[x.ToString().Length - 1 - i])
+                {
+                    palendrome = false; 
+                    break;
+                }
+            }
+            return palendrome;
+        }
+    }
+}
---
+++ Palindrome_Number/Palindrome_Number/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Palindrome_Number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Palindrome_Number")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("975891b8-8c49-49ab-84ad-3eeae0665446")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
